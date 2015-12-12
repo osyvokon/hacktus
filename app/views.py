bf34b197f8557d4b297383ef9ef4fa0467870ffc
marina <m.chernyavska@gmail.com>
2015-12-12 14:17:51 +0100

    added login form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
 from app import app
-
+from .forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Marina'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for GitHub="%s", remember_me=%s' %
+              (form.gh_login.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
