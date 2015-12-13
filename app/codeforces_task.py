0696397bc13f34ca58ed95fd467b6e6db8472924
marina <m.chernyavska@gmail.com>
2015-12-13 11:17:18 +0100

    added CF weekly and monthly

--- app/codeforces_task.py
+++ app/codeforces_task.py
@@ -10,6 +10,7 @@ class CodeforcesProvider:
         self.stats = self.db.codeforces.by_day
         self.last_record = self.refresh_last_record()
         self.url = "http://codeforces.com/api/user.status?handle={}&from=1&count={}"
+        self.refresh_submissions()
 
     def refresh_last_record(self):
         cr = self.collection.find({'user': self.username}).sort([('ts', -1)]).limit(1)
@@ -38,13 +39,13 @@ class CodeforcesProvider:
                 ts = datetime.date.fromtimestamp(timestamp).toordinal()
                 if last_ts is None or last_ts < ts:
                     result = True
-        print "Codeforces - checking for new submissions: {}".format(self.username)
+        print("Codeforces - checking for new submissions: {}".format(self.username))
         return result
 
     def refresh_submissions(self):
         if not self.has_new_submissions():
             return
-        print "Codeforces - refreshing submissions for: {}".format(self.username)
+        print("Codeforces - refreshing submissions for: {}".format(self.username))
         url = self.url.format(self.username, 500)
         data = requests.get(url).json()
         saved = 0
@@ -80,25 +81,44 @@ class CodeforcesProvider:
             result.append(doc)
         return result
 
+    def get_submissions_between(self, start_date, end_date):
+        since = datetime.datetime(start_date.year, start_date.month, start_date.day)
+        until = datetime.datetime(end_date.year, end_date.month, end_date.day) + datetime.timedelta(days=1)
+        cr = self.collection.find({'$and': [
+            {'ts': {'$gte': since.toordinal()}},
+            {'ts': {'$lt': until.toordinal()}},
+            {'user': self.username}
+        ]}).sort([('ts', -1)])
+        result = []
+        for doc in cr:
+            result.append(doc)
+        return result
 
-def get_stats_for_day(name, dt):
-    pr = CodeforcesProvider(name)
-    result = {
-        'submissions': 0,
-        'passed': 0,
-        'failed': 0
-    }
-    print "Codeforces - getting statistics"
-    pr.refresh_submissions()
+    def subs_to_stats(self, subs):
+        result = {
+            'submissions': 0,
+            'passed': 0,
+            'failed': 0
+        }
+        if subs is not None and len(subs) > 0:
+            for sub in subs:
+                if 'success' in sub and sub['success']:
+                    result['passed'] += 1
+                else:
+                    result['failed'] += 1
+                result['submissions'] += 1
+        return result
 
-    subs = pr.get_submissions(dt)
-    if subs is not None and len(subs) > 0:
-        for sub in subs:
-            if 'success' in sub and sub['success']:
-                result['passed'] += 1
-            else:
-                result['failed'] += 1
-            result['submissions'] += 1
-    pr.stats.update({'user': name, 'dt': dt.toordinal()}, {'$set': {'stats': result}}, upsert=True)
-    return result
+    def get_stats_for_day(self, name, dt):
+        return self.subs_to_stats(self.get_submissions(dt))
+
+    def get_stats_for_week(self, name):
+        end_date = datetime.date.today()
+        start_date = end_date - datetime.timedelta(days=7)
+        return self.subs_to_stats(self.get_submissions_between(start_date, end_date))
+
+    def get_stats_for_month(self, name):
+        end_date = datetime.date.today()
+        start_date = end_date - datetime.timedelta(days=7)
+        return self.subs_to_stats(self.get_submissions_between(start_date, end_date))
 
